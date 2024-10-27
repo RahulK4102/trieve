18930148a4f91aef7bdc4e30190d4be930373747
cdxker <fdenzell@gmail.com>
2024-10-27 15:24:03 -0700

    feature: set defualt values in search UI

--- frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
+++ frontends/dashboard/src/pages/dataset/PublicPageSettings.tsx
@@ -1,4 +1,4 @@
-import { createSignal, useContext } from "solid-js";
+import { useContext } from "solid-js";
 import { createToast } from "../../components/ShowToasts";
 import { ApiRoutes } from "../../components/Routes";
 import { DatasetContext } from "../../contexts/DatasetContext";
@@ -106,7 +106,7 @@ export const PublicPageSettings = () => {
           </label>
           <input
             placeholder="https://cdn.trieve.ai/favicon.ico"
-            value={""}
+            value={extra_params.brandLogoImgSrcUrl || ""}
             onInput={(e) => {
               setExtraParams("brandLogoImgSrcUrl", e.currentTarget.value);
             }}
@@ -118,8 +118,8 @@ export const PublicPageSettings = () => {
             Brand Name
           </label>
           <input
-            placeholder="https://example.com/openapi.json"
-            value={""}
+            placeholder="Trieve"
+            value={extra_params.brandName || ""}
             onInput={(e) => {
               setExtraParams("brandName", e.currentTarget.value);
             }}
@@ -132,9 +132,9 @@ export const PublicPageSettings = () => {
           </label>
           <input
             placeholder="light"
-            value={""}
+            value={extra_params.theme || ""}
             onInput={(e) => {
-              setExtraParams("theme", e.currentTarget.value);
+              setExtraParams("theme", e.currentTarget.value === "dark" ? "dark" : "light");
             }}
             class="block w-full rounded border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
           />
@@ -145,7 +145,7 @@ export const PublicPageSettings = () => {
           </label>
           <input
             placeholder="#CB53EB"
-            value={""}
+            value={extra_params.accentColor || ""}
             onInput={(e) => {
               setExtraParams("accentColor", e.currentTarget.value);
             }}
@@ -161,8 +161,8 @@ export const PublicPageSettings = () => {
               Problem Link
             </label>
             <input
-              placeholder="https://example.com/openapi.json"
-              value={""}
+              placeholder="mailto:humans@trieve.ai"
+              value={extra_params.problemLink || ""}
               onInput={(e) => {
                 setExtraParams("problemLink", e.currentTarget.value);
               }}
@@ -176,11 +176,10 @@ export const PublicPageSettings = () => {
               Responsive View
             </label>
             <input
-              placeholder="Search..."
-              value={""}
+              checked={extra_params.responsive || false}
               type="checkbox"
               onInput={(e) => {
-                setExtraParams("responsive", e.currentTarget.value);
+                setExtraParams("responsive", e.currentTarget.value === "true");
               }}
               class="block w-4 rounded border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
             />
@@ -190,11 +189,10 @@ export const PublicPageSettings = () => {
               Analytics
             </label>
             <input
-              placeholder="Search..."
-              value={""}
+              checked={extra_params.analytics || true}
               type="checkbox"
-              onInput={(e) => {
-                setExtraParams("analytics", e.currentTarget.value);
+              onChange={(e) => {
+                setExtraParams("analytics", e.currentTarget.checked);
               }}
               class="block w-4 rounded border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
             />
@@ -205,10 +203,10 @@ export const PublicPageSettings = () => {
             </label>
             <input
               placeholder="Search..."
-              value={""}
+              checked={extra_params.suggestedQueries || true}
               type="checkbox"
-              onInput={(e) => {
-                setExtraParams("suggestedQueries", e.currentTarget.value);
+              onChange={(e) => {
+                setExtraParams("suggestedQueries", e.currentTarget.checked);
               }}
               class="block w-4 rounded border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
             />
@@ -219,10 +217,10 @@ export const PublicPageSettings = () => {
             </label>
             <input
               placeholder="Search..."
-              value={""}
+              checked={extra_params.chat || true}
               type="checkbox"
-              onInput={(e) => {
-                setExtraParams("chat", e.currentTarget.value);
+              onChange={(e) => {
+                setExtraParams("chat", e.currentTarget.checked);
               }}
               class="block w-4 rounded border border-neutral-300 px-3 py-1.5 shadow-sm placeholder:text-neutral-400 focus:outline-magenta-500 sm:text-sm sm:leading-6"
             />
@@ -240,7 +238,7 @@ export const PublicPageSettings = () => {
             value={() => {
               return extra_params?.searchOptions || {}
             }}
-            onError={(error) => { }}
+            onError={(_) => { }}
           />
         </div>
         <div class="p-2 space-y-1.5">
@@ -288,6 +286,3 @@ export const PublicPageSettings = () => {
     </div>
   );
 };
-function useDatasetServerConfig() {
-  throw new Error("Function not implemented.");
-}
